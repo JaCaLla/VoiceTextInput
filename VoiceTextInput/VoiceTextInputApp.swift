ebc45d90d4c818e899b353b11e8b67a0e5ce607a
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-14 09:07:14 +0100

    Base project for starting with

---
+++ VoiceTextInput/VoiceTextInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTextInputApp.swift
+//  VoiceTextInput
+//
+//  Created by Javier Calatrava on 14/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTextInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
