9e139d33cee9efbc66d5b89c5d4318ada39e9c6f
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-17 16:46:16 +0100

    Created blank project

---
+++ CombineAPIRest/CombineAPIRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAPIRestApp.swift
+//  CombineAPIRest
+//
+//  Created by Javier Calatrava on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CombineAPIRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
